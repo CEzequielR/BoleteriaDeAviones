9773c573a2d5bc29dd9ddea56d8c037c8ca10ee7
CEzequielR <checheze@hotmail.com.ar>
2024-06-04 18:38:18 -0300

    first commit

---
+++ src/main/java/controlador/Inicio.java
@@ -0,0 +1,18 @@
+
+package controlador;
+
+import java.util.Random;
+import javax.swing.JFrame;
+import vista.Autenticacion;
+
+
+public class Inicio {
+    public static void main(String[] args) {
+        Autenticacion aute1 = new Autenticacion();
+        aute1.setVisible(true);
+        aute1.setLocationRelativeTo(null);
+        aute1.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        
+    }
+}
